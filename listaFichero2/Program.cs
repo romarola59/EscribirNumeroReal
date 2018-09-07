68284ad41c5cbf3c2ebfd619bb6e31e05f030e52
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-06 22:28:24 -0300

    EscribirNUmeroReal

--- listaFichero2/Program.cs
+++ listaFichero2/Program.cs
@@ -2,11 +2,26 @@
 
 namespace listaFichero2
 {
-    class Program
+    class Program 
     {
-        static void Main(string[] args)
+        public static void EscribirNumeroReal(float n)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(n.ToString("#.###"));
         }
+        public static void Main()
+        {
+            float x;
+            x = 5.1f;
+            Console.WriteLine("El primer numero real es: ");
+            EscribirNumeroReal(x);
+            Console.WriteLine("Y otro distinto es: ");
+            EscribirNumeroReal (2.3f);
+            Console.ReadKey();
+
+        }
+
+         
+
+
     }
 }
